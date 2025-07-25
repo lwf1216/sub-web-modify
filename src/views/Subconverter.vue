aae7bdfa4da945854b3fb331fa27467e366acdc6
lwf1216 <148003515+lwf1216@users.noreply.github.com>
2025-07-25 22:56:43 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -337,15 +337,41 @@ export default {
           "dlj.tf": "https://dlj.tf/short",
           "suo.yt": "https://suo.yt/short",
         },
-        customBackend: {
-          "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
-          "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
-          "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
-          "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
-          nameless13提供: "https://www.nameless13.com",
-          subconverter作者提供: "https://sub.xeton.dev",
-          "sub-web作者提供": "https://api.wcc.best",
-        },
+... // 省略其他代码
+
+customBackend: {
+      "自用专属后端": "https://hk.310615.xyz/",// 为您的后端取名
+      "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
+      "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
+      "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
+      "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
+      "nameless13提供": "https://www.nameless13.com",
+      "subconverter作者提供": "https://sub.xeton.dev",
+      "sub-web作者提供": "https://api.wcc.best",
+  },
+  backendOptions: [
+      { value: "https://subapi.cmliussss.hidns.vip" }, // 添加您的后端地址
+      { value: "https://subapi.cmliussss.net" },
+      { value: "https://subapi.fxxk.dedyn.io" },
+      { value: "https://url.v1.mk" },
+      { value: "https://sub.d1.mk" },
+      { value: "https://www.nameless13.com" },
+      { value: "https://sub.xeton.dev" },
+      { value: "https://api.wcc.best" },
+  ],
+
+  ... // 省略其他代码
+  
+  form: { // 默认表单配置
+  sourceSubUrl: "",
+  clientType: "",
+  customBackend: this.getUrlParam() == "" ? "https://subapi.cmliussss.hidns.vip" : this.getUrlParam(),// 替换为您的后端地址
+  shortType: "https://v1.mk/short",
+  remoteConfig: "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online.ini",// 替换为您常用的订阅转换配置文件地址
+  excludeRemarks: "",
+  includeRemarks: "",
+
+  ... // 省略其他代码,
         backendOptions: [
           { value: "https://subapi.cmliussss.net" },
           { value: "https://subapi.fxxk.dedyn.io" },
